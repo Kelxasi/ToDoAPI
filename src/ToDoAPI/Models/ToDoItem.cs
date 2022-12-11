58d2177dfa338c915b41e561110990e382a29958
Nurettin <107817157+Kelxasi@users.noreply.github.com>
2022-12-11 03:02:35 +0300

    stupid commit message

--- src/ToDoAPI/Models/ToDoItem.cs
+++ src/ToDoAPI/Models/ToDoItem.cs
@@ -1,12 +1,10 @@
 namespace ToDoAPI.Models
 {
+    //This is a comment
     public class ToDoItem
     {
         public int Id { get; set; }
         
         public string? Task { get; set; }
-
-        
-
     }
 }
